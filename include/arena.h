2fb554a6ad333180e01cc95e870090d7945b1764
HectorDY <guilherme.fnemer@gmail.com>
2026-08-05 19:37:08 -0300

    Update arena.h

--- include/arena.h
+++ include/arena.h
@@ -1,1 +1,16 @@
+#ifndef ARENA_H
+#define ARENA_H
 
+#include <stddef.h>
+
+typedef struct {
+    char *buffer;
+    size_t size;
+    size_t offset;
+} Arena;
+
+void arena_init(Arena *a, size_t size);
+void *arena_alloc(Arena *a, size_t size);
+void arena_destroy(Arena *a);
+
+#endif
